7779e4adb756512c63a45d03eece5390e1ed045b
S4NGAL <41579632+S4NGAL@users.noreply.github.com>
2022-12-22 16:37:36 +0300

    rock part completed

--- pages/rock_paper.js
+++ pages/rock_paper.js
@@ -8,3 +8,20 @@ function getComputerChoice() {
     return moves[a];
 }
 
+//ps for playerSelection cs for computerSelection
+function playRound(ps, cs){
+    switch (ps){
+        case "rock":
+            switch (cs) {
+                case "rock":
+                    return "Rock equals Rock no winner"
+                case "paper":
+                    return "Rock get lost against Paper"
+                case "scissors":
+                    return "Rock breaks Scissors"
+            }
+            break;
+
+    }
+}
+

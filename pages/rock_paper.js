a28eedeecee9010d1a3c50344928624daa708588
S4NGAL <41579632+S4NGAL@users.noreply.github.com>
2022-12-22 22:14:29 +0300

    create basic game

--- pages/rock_paper.js
+++ pages/rock_paper.js
@@ -14,14 +14,36 @@ function playRound(ps, cs){
         case "rock":
             switch (cs) {
                 case "rock":
-                    return "Rock equals Rock no winner"
+                    return "Rock equals Rock no winner";
                 case "paper":
-                    return "Rock get lost against Paper"
+                    return "Rock get lost against Paper Player lose";
                 case "scissors":
-                    return "Rock breaks Scissors"
+                    return "Rock breaks Scissors Player Wins";
             }
             break;
+        
+        case "paper":
+            switch (cs) {
+                case "paper":
+                    return "Paper equals paper no winner";
+                case "rock":
+                    return "Paper beats Rock Player wins";
+                case "scissors":
+                    return "scissors cuts paper Player LOSE";
+            }
+            break;
+        
+        case "scissors":
+            switch(cs){
+                case "scissors":
+                    return "Scissors equal to Scissors no winner";
+                case "rock":
+                    return "Rock breaks scissors Player LOSE";
+                case "paper":
+                    return "Scissors cuts paper Player Wins";
+            }
 
     }
 }
 
+console.log(playRound(getComputerChoice(), getComputerChoice()));

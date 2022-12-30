96064776b7504efeee522645a7b29f3bdef79c7e
S4NGAL <41579632+S4NGAL@users.noreply.github.com>
2022-12-30 13:42:16 +0300

    add buttons

--- pages/rock_paper.js
+++ pages/rock_paper.js
@@ -1,4 +1,6 @@
 const moves = ["rock", "paper", "scissors"]
+let winsOfPlayer = 0;
+let winsOfComputer = 0;
 
 Math.floor(Math.random() * 4);
 
@@ -15,10 +17,15 @@ function playRound(ps, cs){
         case "rock":
             switch (cs) {
                 case "rock":
+                    checkStatus();
                     return "Rock equals Rock no winner";
                 case "paper":
+                    winsOfComputer++;
+                    checkStatus();
                     return "Rock get lost against Paper Player lose";
                 case "scissors":
+                    winsOfPlayer++;
+                    checkStatus();
                     return "Rock breaks Scissors Player Wins";
             }
             break;
@@ -26,10 +33,15 @@ function playRound(ps, cs){
         case "paper":
             switch (cs) {
                 case "paper":
+                    checkStatus();
                     return "Paper equals paper no winner";
                 case "rock":
+                    winsOfPlayer++;
+                    checkStatus();
                     return "Paper beats Rock Player wins";
                 case "scissors":
+                    winsOfComputer++;
+                    checkStatus();
                     return "scissors cuts paper Player LOSE";
             }
             break;
@@ -37,17 +49,45 @@ function playRound(ps, cs){
         case "scissors":
             switch(cs){
                 case "scissors":
+                    checkStatus();
                     return "Scissors equal to Scissors no winner";
                 case "rock":
+                    winsOfComputer++;
+                    checkStatus();
                     return "Rock breaks scissors Player LOSE";
                 case "paper":
+                    winsOfPlayer++;
+                    checkStatus();
                     return "Scissors cuts paper Player Wins";
             }
+    
+        }
 
-    }
 }
 
 
+
+function checkStatus() {
+
+    if (winsOfComputer === 5 || winsOfPlayer === 5) {
+        if(winsOfPlayer > winsOfComputer){
+            playerWins();
+        }else {
+            computerWins();
+        }
+    }
+
+    console.log("Player: " + winsOfPlayer);
+    console.log("Computer: " + winsOfComputer);
+}
+
+function playerWins(){
+    console.log("Player Wins!!!");
+}
+
+function computerWins(){
+    console.log("Computer Wins!!!");
+}
 
 function rock(){
     let playerChoice = "rock";
@@ -63,3 +103,15 @@ function scissors(){
     let playerChoice = "scissors";
     console.log(playRound(playerChoice, getComputerChoice()));
 }
+
+document.getElementById("btnRck").addEventListener("click", function(){
+    rock();
+});
+
+document.getElementById("btnSci").addEventListener("click", function(){
+    scissors();
+});
+
+document.getElementById("btnPpr").addEventListener("click", function(){
+    paper();
+});
